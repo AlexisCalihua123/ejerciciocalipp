18094717669ad66ae7bc13e55266c24c628e3499
Leal Calihua Brayan <64117204+AlexisCalihua123@users.noreply.github.com>
2020-04-21 22:45:53 -0500

    Initial commit

---
+++ app/src/main/java/com/edu/tesoem/isc/alexis/ejerciciocali/MainActivity.java
@@ -0,0 +1,14 @@
+package com.edu.tesoem.isc.alexis.ejerciciocali;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
